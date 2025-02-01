21b563f5f1fe521e7923d80da253ba22483b6476
Samim Hossen <85032804+samim5274@users.noreply.github.com>
2025-02-01 18:47:26 +0600

    u

--- app/Http/Controllers/Account/AccountController.php
+++ app/Http/Controllers/Account/AccountController.php
@@ -23,7 +23,6 @@ class AccountController extends Controller
         $accounts = new Account();
 
         $senderId = Auth::guard('admin')->user()->id;
-        
         $senderType = 1;
         $receriverType = 2;
         
@@ -35,37 +34,36 @@ class AccountController extends Controller
         ]);
 
         $receiverid = $request->has('cbxAccount') ? $request->get('cbxAccount') : '';
+        $pass = $request->has('txtPassword') ? $request->get('txtPassword') : '';
         
-        $user = Auth::guard('admin')->attempt(['password'=>$request->txtPassword]);
-        if(!$user){
-            return redirect()->back()->with('error', 'Invalid password');
+        $userAdmin = Admin::where('id', $senderId)->first();
+        $data = Hash::check($pass, $userAdmin->password);
+        
+        if(!$data){
+            return redirect()->back()->with('error', 'Password is incorrect. Please enter correct password.');
         }
         else
         {
-            return 'password is correct';
-        }
+            if($senderId == $receiverid){
+                return redirect()->back()->with('error', 'You can select your own account for money transer. Please select another account.');
+            }
+            else
+            {
+                $account = $request->has('cbxAccount') ? $request->get('cbxAccount') : '';
+                $amount = $request->has('txtAmount') ? $request->get('txtAmount') : '';
+                $remark = $request->has('txtRemark') ? $request->get('txtRemark') : '';
 
-        // $admin = Admin::where('id', $senderId)->where('password', $pass)->first();
-
-        // if(!$admin){
-        //     return redirect()->back()->with('error', 'Password is incorrect. Please enter correct password.');
-        // }
-        // else
-        // {
-        //     if($senderId == $receiverid){
-        //         return redirect()->back()->with('error', 'You can select your own account for money transer. Please select another account.');
-        //     }
-        //     else
-        //     {
-        //         $date = date('Y-m-d');
-        //         $account = $request->has('cbxAccount') ? $request->get('cbxAccount') : '';
-        //         $amount = $request->has('txtAmount') ? $request->get('txtAmount') : '';
-        //         $remark = $request->has('txtRemark') ? $request->get('txtRemark') : '';
+                $accounts->date = date('Y-m-d');
+                $accounts->amount = $amount;
+                $accounts->sender_id = $senderId;
+                $accounts->senderType = $senderType;
+                $accounts->receiver_id = $account;
+                $accounts->receiverType = $receriverType;
+                $accounts->remark = $remark;
                 
-        
-        //         // return view('account.money-send');
-        //          return 'money send successfully';
-        //     } 
-        // }               
+                $accounts->save();
+                return redirect()->route('account.view')->with('success', 'Money transfer successfully.');
+            } 
+        }               
     }
 }
